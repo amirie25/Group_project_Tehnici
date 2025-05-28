cf52cec46e6ab63a9269b727f3a030ca63de0e9e
unknown <miriam.anchidin@emanuel.ro>
2025-05-28 20:11:37 +0300

    Adaugare canvas

---
+++ scriptCanvas.js
@@ -0,0 +1,13 @@
+let borderWidth = 0
+let borderHeight = 0
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    borderWidth = windowWidth
+    borderHeight = windowHeight
+}
+
+function draw() {
+    background("#ffdfff")
+    drawColorfulBoxes()
+}
